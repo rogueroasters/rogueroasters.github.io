428f333c4920b5f88402a9eaf88e6a9b85c4fb33
rogueroasters <support@rogueroasters.com>
2017-10-21 12:53:07 -0700

    move build to root

--- src/components/slide_2.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import '../assets/css/slide_2.css'
-const Slide2 = () => (<div className="parallax2"></div>)
-export default Slide2
--- src/components/slide_4.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import '../assets/css/slide_4.css'
-const Slide4 = () => (<div className="parallax3"></div>)
-export default Slide4
